5f8aef9ef46043d27877b11cdde5e7dacdfbb06f
Patrik Stutz <patrik.stutz@gmail.com>
2020-11-24 02:24:25 +0100

    put everything into an fhs to work around the binary checking its path (thanks to tinywrkb)

--- default.nix
+++ default.nix
@@ -1,38 +1,43 @@
-{fetchurl,stdenv,autoPatchelfHook,dpkg,lib,makeWrapper}:
-stdenv.mkDerivation rec {
+{fetchurl,stdenv,buildFHSUserEnv,dpkg,writeScript,tree,lib}:
+let
+  package = stdenv.mkDerivation rec {
 
-  pname = "insync-headless";
-  version = "3.0.4.10602";
+    pname = "insync-headless";
+    version = "3.0.4.10602";
 
-  src = 
-    if stdenv.hostPlatform.system == "x86_64-linux" then
-        fetchurl {
-            url = "https://d2t3ff60b2tol4.cloudfront.net/builds/insync-headless_3.0.4.10602-buster_amd64.deb";
-            sha256 = "0ja6jzd2q3na6z22jhh5r1md12imr3y85mbigbr52xav4lw2y18j";
-        }
-    else
-      throw "${pname}-${version} is not supported on ${stdenv.hostPlatform.system}";
+    src = 
+      if stdenv.hostPlatform.system == "x86_64-linux" then
+          fetchurl {
+              url = "https://d2t3ff60b2tol4.cloudfront.net/builds/insync-headless_3.0.4.10602-buster_amd64.deb";
+              sha256 = "0ja6jzd2q3na6z22jhh5r1md12imr3y85mbigbr52xav4lw2y18j";
+          }
+      else
+        throw "${pname}-${version} is not supported on ${stdenv.hostPlatform.system}";
 
-  unpackCmd = "${dpkg}/bin/dpkg-deb -x $curSrc .";
+    unpackCmd = "${dpkg}/bin/dpkg-deb -x $curSrc .";
 
-  nativeBuildInputs = [
-    autoPatchelfHook
-  ];
+    installPhase = ''
+      mkdir -p $out
+      cp -r . $out/
+    '';
 
-  buildInputs = [
-    makeWrapper
-  ];
-
-  installPhase = ''
-    mkdir -p $out
-    cp -r . $out/
-    makeWrapper $out/lib/insync/insync-headless $out/bin/insync-headless --set LC_TIME C
-  '';
-
-  meta = with lib; {
-    homepage = "https://www.insynchq.com";
-    description = "Google Drive & OneDrive Syncing on Linux, Windows & Mac";
-    platforms = [ "x86_64-linux" ];
-    license = licenses.unfree;
+    meta = with lib; {
+      homepage = "https://www.insynchq.com";
+      description = "Google Drive & OneDrive Syncing on Linux, Windows & Mac";
+      platforms = [ "x86_64-linux" ];
+      license = licenses.unfree;
+    };
   };
-}
+in
+  # putting it in a fhs is sadly necessary, because the insync binary checks the path it is installed at.
+  # this was found out by tinywrkb (https://forums.insynchq.com/t/flatpak-related-issues/16016/11)
+  buildFHSUserEnv rec {
+    name = "insync-headless";
+    targetPkgs = pkgs: [
+      package
+      pkgs.zlib
+    ];
+    runScript = writeScript "${name}-wrapper" ''
+      exec /usr/bin/insync-headless $@
+    '';
+  }
